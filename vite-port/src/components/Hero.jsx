969ef4415b936cea850f0572e77a910c6694debd
SIBIARASU D <sibiarasu.cs20@bitsathy.ac.in>
2022-12-24 11:42:03 +0530

    first commit

---
+++ vite-port/src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from '../style'
+import { discount, robot } from '../assets'
+
+const Hero = () => (
+  <section id='home' className={`flex md:flex-row fex-col ${styles.paddingY}`}>
+    <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+      <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+          <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
+            <p className={`${styles.paragraph} ml-2`}>
+              <span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">20%</span> Discount For{" "}
+              <span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">1 Month</span> Account
+            </p>
+      </div>
+    </div>
+
+  </section>
+  
+)
+
+export default Hero
